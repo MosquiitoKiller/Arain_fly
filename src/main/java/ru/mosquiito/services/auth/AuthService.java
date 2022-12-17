a65b16d1c154cd7df9b203112e13862e64b8db1e
MosquiitoKiller <mosquiitokiller@gmail.com>
2022-12-17 14:48:57 +0300

    Добавлена авторизация

--- src/main/java/ru/mosquiito/services/auth/AuthService.java
+++ src/main/java/ru/mosquiito/services/auth/AuthService.java
@@ -1,19 +1,30 @@
 package ru.mosquiito.services.auth;
 
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.AuthenticationException;
+import io.micronaut.security.authentication.AuthenticationFailed;
+import io.micronaut.security.authentication.AuthenticationRequest;
+import io.micronaut.security.authentication.AuthenticationResponse;
 import jakarta.inject.Inject;
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import ru.mosquiito.domain.Account;
 import ru.mosquiito.domain.AccountConfirmation;
+import ru.mosquiito.domain.Role;
 import ru.mosquiito.domain.enums.AccountStatus;
 import ru.mosquiito.dto.RegistrationDto;
 import ru.mosquiito.dto.SimpleResponseDto;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 
-public class AuthService implements IRegistrationService {
+public class AuthService implements IRegistrationService, ILoginService {
 
     private static final Logger log = LoggerFactory.getLogger(AuthService.class);
 
@@ -52,5 +63,26 @@ public class AuthService implements IRegistrationService {
         accountDataAccess.save(account);
         mailSender.send(account);
         return new SimpleResponseDto(true, "OK");
+    }
+
+    @Override
+    public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest,
+                                                          AuthenticationRequest<?, ?> authenticationRequest) {
+        return Flux.create(emitter -> {
+            String username = (String) authenticationRequest.getIdentity();
+            String password = (String) authenticationRequest.getSecret();
+            Account account = accountDataAccess.findByEmail(username);
+            if (account != null &&
+                    encoder.matches(password, account.getPassword()) &&
+                    account.getAccountStatus() == AccountStatus.ACTIVE) {
+                Collection<String> roles = account.getRoles().stream().map(Role::getName).toList();
+                emitter.next(
+                        AuthenticationResponse.success((String) authenticationRequest.getIdentity(),
+                                roles));
+                emitter.complete();
+            } else {
+                emitter.error(new AuthenticationException(new AuthenticationFailed()));
+            }
+        }, FluxSink.OverflowStrategy.ERROR);
     }
 }
---
+++ src/main/java/ru/mosquiito/services/auth/ILoginService.java
@@ -0,0 +1,6 @@
+package ru.mosquiito.services.auth;
+
+import io.micronaut.security.authentication.AuthenticationProvider;
+
+public interface ILoginService extends AuthenticationProvider {
+}
